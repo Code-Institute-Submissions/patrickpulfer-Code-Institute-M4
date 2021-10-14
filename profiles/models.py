8ec9223521942508fbb2b3aff0d83cb75986b342
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-14 19:06:36 +0100

    Feature: Extend Django users to include extra fields like bio, avatar image

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, related_name='profile', on_delete=models.CASCADE)
+    avatar = models.ImageField(
+        default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(blank=True)
+    signature = models.TextField(blank=True)
+    premium = models.BooleanField(default=False)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
