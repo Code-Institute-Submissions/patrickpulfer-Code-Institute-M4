41653d64c459cc44e85923bb0b2e550f81198e8b
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-14 19:20:15 +0100

    Feature: Add media, static and templates folders

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def profiles(request, username):
+    profile = get_object_or_404(User, username=username)
+    context = {
+        'profile': profile,
+    }
+    return render(request, 'profiles/index.html', context)
