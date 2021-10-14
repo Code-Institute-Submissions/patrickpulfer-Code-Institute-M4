c775d367e461d887e96cb343478bbcbc7546fe21
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-14 20:15:54 +0100

    Feature: Add basic details to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
-from django.contrib.auth.models import User
 
 
 def profiles(request, username):
-    profile = get_object_or_404(User, username=username)
+    user = get_object_or_404(User, username=username)
+    profile = get_object_or_404(Profile, user_id=user.id)
     context = {
-        'profile': profile,
+        'user_data': user,
+        'user_profile': profile,
     }
     return render(request, 'profiles/index.html', context)
