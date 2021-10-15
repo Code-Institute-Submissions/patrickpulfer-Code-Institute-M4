0d770a5352dc2ef6a7b2e0dc5fd70e6f9f3a1687
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-15 01:24:25 +0100

    Fix: Fix formatting issues with profiles template

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# from config.settings import MEDIA_ROOT
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
