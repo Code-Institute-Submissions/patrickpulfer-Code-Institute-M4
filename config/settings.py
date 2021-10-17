b44163276e57ec5ed25e4dd62e5fdfde5a058dba
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-17 23:06:07 +0100

    Security Fix: Add python-dotenv, generated a new secret key and removed deprecated one from settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'profiles',
     'forum',
 ]
+
 
 # django-allauth config
 SITE_ID = 1
