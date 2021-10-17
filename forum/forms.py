2c432fcd23986a2d63ce55d41bc07c50b8099d0a
patrickpulfer <patrick.pulfer1@gmail.com>
2021-10-17 01:28:19 +0100

    Feature: Add ability to comment discussion posts

--- forum/forms.py
+++ forum/forms.py
@@ -3,10 +3,17 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['visible'].initial = True
+
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ('body', 'visible')
         labels = {'body': 'Your comment', }
         widgets = {
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            # 'discussion': forms.HiddenInput,
+            'visible': forms.HiddenInput,
         }
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Forum, Discussion, Comment
 from django.contrib.auth.models import User
@@ -14,41 +15,37 @@ def forum(request, param_forum_name):
     forum = get_object_or_404(Forum, forum_name=param_forum_name)
     discussions = Discussion.objects.filter(forum=forum.id)
 
-    #comments = Comment.objects.filter(discussions)
-    # print(list(discussions))
-    #comments = get_object_or_404(Discussion, id=discussions)
-
-#comments = Comment.objects.filter(discussion=discussions)
-
-# poster = Discussion.objects.filter(poster=User.id)
-
-# print(comments)
-
-
-# print("Forum Topic: ")
-    # print(list(discussions))
-    # int("Model:")
-    # print(list(Forum.forum_name))
-    # print(Forum)
-    # discussions = Discussion.objects.filter(forum=forum)
-
     context = {
         'forum_data': forum,
         'discussions_data': discussions,
-        #    'comments_data': comments
     }
     return render(request, 'forum/forum_view.html', context)
 
 
 def discussion_view(request, param_discussion):
-    discussion = get_object_or_404(Discussion, title=param_discussion)
-    comments = Comment.objects.filter(discussion=discussion.id)
 
-    comment_form = CommentForm(instance=request.user.profile)
+    if request.method == 'POST' and request.user.is_authenticated:
+        comment_form = CommentForm(request.POST)
 
-    context = {
-        'discussion': discussion,
-        'comments': comments,
-        'comment_form': comment_form
-    }
-    return render(request, 'forum/discussion_view.html', context)
+        if comment_form.is_valid():
+
+            obj = comment_form.save(commit=False)
+
+            discussion = get_object_or_404(Discussion, title=param_discussion)
+            obj.discussion = discussion
+            obj.poster = request.user
+            obj.save()
+            return HttpResponseRedirect('/forum/discussion/%s' % discussion.title)
+
+    else:
+        discussion = get_object_or_404(Discussion, title=param_discussion)
+        comments = Comment.objects.filter(discussion=discussion.id)
+        comment_form = CommentForm(instance=request.user.profile)
+
+        context = {
+            'discussion': discussion,
+            'comments': comments,
+            'comment_form': comment_form
+        }
+
+        return render(request, 'forum/discussion_view.html', context)
